3b5877ab92daa20f956498cffb165dd58122a2f0
pedro H costa <137086984+pedrocosta2008@users.noreply.github.com>
2023-08-21 21:44:53 -0300

    Create index.js
    exerc - Pedro Costa

---
+++ index.js
@@ -0,0 +1,34 @@
+/*
+*pedro costa
+*primeiro coment
+*/
+console.log("Olá Mundo");
+//prompt("digite seu nome!");
+
+//variaveis
+const variavel1A = 100;
+const variavel1B = variavel1A + 50
+console.log(variavel1B);
+
+let numero = 50;
+console.log(numero);
+numero = 80
+console.log(numero);
+
+//const nome = "PEDRO"
+//const idade = 15
+
+//console.log("Olá!", "Meu nome é ",nome, "e eu tenho", idade, "anos.");
+
+//Nome
+let name = "piter";
+
+//sobrenome
+let sobrenome = "costa";
+
+//idade
+let age = 15;
+
+//student?
+let student = true;
+console.log("meu é", name,sobrenome, "eu tenho", age, "anos." , "Estudo no colegio SENACRS?",student );
